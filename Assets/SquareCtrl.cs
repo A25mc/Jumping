85cc94546d01a37a00a23dc02d663e81b1eac22d
A25mc <a.sd2357ch@gmail.com>
2023-05-08 14:32:44 +0900

    外での編集分

--- Assets/SquareCtrl.cs
+++ Assets/SquareCtrl.cs
@@ -6,7 +6,9 @@ public class SquareCtrl : MonoBehaviour
 {
     Rigidbody2D rigid2d;
     float jumpf;
-    float maxjumpf = 800.0f;
+    float maxjumpf = 600.0f;
+    float warkf = 30.0f;
+    float maxwalkspeed = 2.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +20,12 @@ public class SquareCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetKey(KeyCode.Space))
         {
-            this.jumpf += 60.0f;
+            this.jumpf += 50.0f;
         }
 
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetKeyUp(KeyCode.Space))
         {
             if (jumpf < this.maxjumpf)
             {
@@ -35,6 +37,19 @@ public class SquareCtrl : MonoBehaviour
                 this.rigid2d.AddForce(transform.up * this.maxjumpf);
                 jumpf = 0;
             }
-        }      
+        }
+
+        int direction = 0;
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction = -1;
+        }
+        
+        float speedx=Mathf.Abs(this.rigid2d.velocity.x);
+
+        if (speedx < this.maxwalkspeed)
+        {
+            this.rigid2d.AddForce(transform.right * direction * this.warkf);
+        }
     }
 }
